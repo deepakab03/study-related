bd6538ec7cd536c8a79c205559c87f6492fab09a
Deepak Abraham <deepakab03-sw@yahoo.com>
2015-03-28 01:12:36 +0530

    dc assignment 1

---
+++ ms/dca1/src/com/deepak/mtech/dc/assignment/event/Event.java
@@ -0,0 +1,13 @@
+package com.deepak.mtech.dc.assignment.event;
+
+import java.util.EventObject;
+
+public class Event extends EventObject{
+
+	private static final long serialVersionUID = 1L;
+
+	public Event(Object source) {
+		super(source);
+	}
+
+}
