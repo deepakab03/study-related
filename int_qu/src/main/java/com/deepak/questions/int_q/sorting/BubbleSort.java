19bf61fd8f61eedb7e61e49eb349b22ff812c62c
abrahd2 <deepak.abraham@emc.com>
2017-05-02 19:30:41 +0530

    added some logging

--- int_qu/src/main/java/com/deepak/questions/int_q/sorting/BubbleSort.java
+++ int_qu/src/main/java/com/deepak/questions/int_q/sorting/BubbleSort.java
@@ -12,11 +12,11 @@ public class BubbleSort implements Sorting {
         int swapCounter = 0;
         for (int i = 0; i < input.length; i++) {
             for (int j = i + 1; j < input.length; j++) {
-                logger.info("Comparision Counter {}", ++opCounter);
+                logger.trace("Comparision Counter {}", ++opCounter);
                 
                 if (input[i] > input[j]) {
                     Sorting.swap(input, i , j);
-                    logger.info("Swap Counter {}", ++swapCounter);
+                    logger.trace("Swap Counter {}", ++swapCounter);
                 }
             }
         }
