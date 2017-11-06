c726689c44045de71fb3dcb1aac0b738da926088
abrahd2 <deepak.abraham@emc.com>
2017-11-06 16:49:37 +0530

    sql question

---
+++ int_qu/src/test/java/com/deepak/questions/int_q/hker_rnk/DeptProfQuestTest.java
@@ -0,0 +1,12 @@
+package com.deepak.questions.int_q.hker_rnk;
+
+/**
+ * TODO to complete with embedded or java db
+ * Find which department has highest average salary and print the name and salary of that department
+ * 
+ * @author Deepak Abraham
+ *
+ */
+public class DeptProfQuestTest {
+
+}
